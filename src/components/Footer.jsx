a439fa5fb73a29d57591e7873ce33798bba3a2ac
Biren07 <139025533+Biren07@users.noreply.github.com>
2025-05-17 13:26:48 +0545

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => (
     <p>
       &copy; 2024{' '}
       <a
-        href="https://aman-kumar-sinha.vercel.app/"
+        href="https://www.linkedin.com/feed/"
         target="_blank"
         rel="noopener noreferrer"
         className="text-blue-400 hover:underline"
